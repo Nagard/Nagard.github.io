f673c9160a4b553ecfe73c5b580227e5cf72ad81
Dragan Milicevic <dmilicevic@gmx.de>
2020-02-01 14:37:55 +0100

    add cook

---
+++ cook-landingpage/s_js/config.js
@@ -0,0 +1,8 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-2_DbueFzcX4', // e.g. us-east-2_uXboG5pAb
+        region: 'us-east-2', // e.g. us-east-2
+		clientId: '6m7mhe02fjtjullr0p817fgkk4' //is this used anywhere?
+    },
+};
+
